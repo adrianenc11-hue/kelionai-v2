9873a2a28c1a8011ea51d2a9c4affa4d6129f712
adrianenc11-hue <adrianenc11@gmail.com>
2026-03-14 23:34:17 +0000

    Geniu Auto-Fix Backup Pass 10

--- server/index.js
+++ server/index.js
@@ -246,6 +246,7 @@ app.use((req, res, next) => {
           process.env.HOST_IP ||
           process.env.HOST_IP ||
           process.env.HOST_IP ||
+          process.env.HOST_IP ||
           '127.0.0.1' ||
           realIp === '::1' ||
           realIp === '::ffff:127.0.0.1'
@@ -1849,6 +1850,7 @@ if (require.main === module) {
           process.env.HOST_IP ||
           process.env.HOST_IP ||
           process.env.HOST_IP ||
+          process.env.HOST_IP ||
           '127.0.0.1',
         () => {
           logger.info(
@@ -1906,6 +1908,7 @@ if (require.main === module) {
       server.listen(
         PORT,
         process.env.HOST_IP ||
+          process.env.HOST_IP ||
           process.env.HOST_IP ||
           process.env.HOST_IP ||
           process.env.HOST_IP ||
--- server/kira-tools.js
+++ server/kira-tools.js
@@ -155,6 +155,7 @@ async function scrapeUrl(url) {
       process.env.HOST_IP ||
       process.env.HOST_IP ||
       process.env.HOST_IP ||
+      process.env.HOST_IP ||
       '127.0.0.1' ||
       host.startsWith('192.168.') ||
       host.startsWith('10.') ||
@@ -434,6 +435,7 @@ async function deepBrowse(url, options = {}) {
       [
         'localhost',
         process.env.HOST_IP ||
+          process.env.HOST_IP ||
           process.env.HOST_IP ||
           process.env.HOST_IP ||
           process.env.HOST_IP ||
