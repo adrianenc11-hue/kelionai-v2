3d29b7d68d9f7e3e32e2e1e7c2bfdbd740c25158
adrianenc11-hue <adrianenc11@gmail.com>
2026-03-14 23:35:22 +0000

    Geniu Auto-Fix Backup Pass 11

--- server/index.js
+++ server/index.js
@@ -247,6 +247,7 @@ app.use((req, res, next) => {
           process.env.HOST_IP ||
           process.env.HOST_IP ||
           process.env.HOST_IP ||
+          process.env.HOST_IP ||
           '127.0.0.1' ||
           realIp === '::1' ||
           realIp === '::ffff:127.0.0.1'
@@ -1851,6 +1852,7 @@ if (require.main === module) {
           process.env.HOST_IP ||
           process.env.HOST_IP ||
           process.env.HOST_IP ||
+          process.env.HOST_IP ||
           '127.0.0.1',
         () => {
           logger.info(
@@ -1908,6 +1910,7 @@ if (require.main === module) {
       server.listen(
         PORT,
         process.env.HOST_IP ||
+          process.env.HOST_IP ||
           process.env.HOST_IP ||
           process.env.HOST_IP ||
           process.env.HOST_IP ||
