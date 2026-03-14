19e1f679663334472dc2145b2ca1a02e59a91c73
adrianenc11-hue <adrianenc11@gmail.com>
2026-03-14 23:50:25 +0000

    Geniu Auto-Fix Backup Pass 23

--- server/index.js
+++ server/index.js
@@ -263,7 +263,7 @@ app.use((req, res, next) => {
           process.env.HOST_IP ||
           process.env.HOST_IP ||
           process.env.HOST_IP ||
-          '127.0.0.1' ||
+          process.env.HOST_IP || "127.0.0.1" ||
           realIp === '::1' ||
           realIp === '::ffff:127.0.0.1'
         )
@@ -1883,7 +1883,7 @@ if (require.main === module) {
           process.env.HOST_IP ||
           process.env.HOST_IP ||
           process.env.HOST_IP ||
-          '127.0.0.1',
+          process.env.HOST_IP || "127.0.0.1",
         () => {
           logger.info(
             {
@@ -1974,7 +1974,7 @@ if (require.main === module) {
           process.env.HOST_IP ||
           process.env.HOST_IP ||
           process.env.HOST_IP ||
-          '127.0.0.1',
+          process.env.HOST_IP || "127.0.0.1",
         () => logger.info({ component: 'Server', port: PORT }, 'KelionAI v2.5 on port ' + PORT + ' (migration failed)')
       );
     });
